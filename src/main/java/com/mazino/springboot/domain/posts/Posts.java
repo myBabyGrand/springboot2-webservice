0565965902580bc94a9acea40996a5de0c5cfe32
mybabygrand <amuro_ray@kakao.com>
2020-11-12 20:23:05 +0900

    chapter 3 finished!

--- src/main/java/com/mazino/springboot/domain/posts/Posts.java
+++ src/main/java/com/mazino/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.mazino.springboot.domain.posts;
 
+import com.mazino.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
